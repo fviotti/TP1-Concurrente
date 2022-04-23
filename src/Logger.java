560b2ddb405a3b46f00f5889975e58e8e62047c2
Brian Gerard <bgerard1062@gmail.com>
2022-04-23 17:48:18 -0300

    logger

---
+++ src/Logger.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+
+public class Logger {
+    //FileReader logger=new FileReader("log.txt");
+    //void log();
+    //void save();
+    
+    public void save(){
+    try
+    {
+    FileWriter write=new FileWriter("log.txt",true);
+    
+    write.write("hola");
+    
+    write.close();
+    }
+    catch(Exception e)
+    {
+    System.out.println("Error al escribir");
+    }
+    
+    }
+}
