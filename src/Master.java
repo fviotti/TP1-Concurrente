61b9ec4d7bc118c0c4a7b0ec07a803d8be4699e0
franco <fviotti@mi.unc.edu.ar>
2022-04-23 16:11:10 -0300

    master_commit_00

---
+++ src/Master.java
@@ -0,0 +1,16 @@
+public abstract class Master {
+    private int id;
+    private int time;
+
+    public Master(int minT, int maxT){
+
+    }
+
+    protected int getTime(){
+        return time;
+    }
+
+    protected int getID(){
+        return id;
+    }
+}
